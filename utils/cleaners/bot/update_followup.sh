73d31dac6b600c8db6c5bb3c2d5ff0d651265c71
Gabriel Garcia <ygabo@pm.me>
2022-05-21 21:04:16 +0000

    Actualización

--- utils/cleaners/bot/update_followup.sh
+++ utils/cleaners/bot/update_followup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+sudo docker exec streamlit_cfk2022 /bin/rm /home/app/utils/cleaners/bot/eliminados/*.xlsx
 sudo docker exec streamlit_cfk2022 python3 /home/app/utils/cleaners/bot/update_xlsx.py
+sudo docker cp streamlit_cfk2022:/home/app/utils/cleaners/bot/eliminados/*.xlsx /home/ubuntu/docker-compose/streamlit/CFK2022/utils/cleaners/bot/eliminados
+
 cd ~/docker-compose/streamlit/CFK2022
 git add .
 git commit -m "Actualización"
