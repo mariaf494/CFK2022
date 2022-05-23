7e0e5e91dfca207b917f42ddf9c0d6a1dc9b07ef
Gabriel Garcia <ygabo@pm.me>
2022-05-23 17:15:58 +0000

    se modica script para actualización de archivos en drive

--- utils/cleaners/bot/update_followup.sh
+++ utils/cleaners/bot/update_followup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 python3 ~/docker-compose/streamlit/CFK2022/utils/cleaners/bot/update_xlsx.py
-cd ~/docker-compose/streamlit/CFK2022
-git add .
-git commit -m "Actualización"
-git push origin master
+
+cd ~/mariana
+./gdrive sync upload ~/docker-compose/streamlit/CFK2022/data/descargables/ 10-vecqL2rtGRXkZ4H6xkI4DY1Z5wr7fw
+./gdrive sync upload ~/docker-compose/streamlit/CFK2022/utils/cleaners/bot/eliminados 1xaulGr1dAuJqx2AwtGRKCcRhm9jQZCUP
+
+#cd ~/docker-compose/streamlit/CFK2022
+#git add .
+#git commit -m "Actualización"
+#git push origin master
