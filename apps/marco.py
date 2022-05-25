aeb9e759aa2932deb6c9c28eab4dab1bf583e27d
mariaf494 <mariaf494@gmail.com>
2022-05-25 16:00:55 -0500

    añadir mejoras al marco

--- apps/marco.py
+++ apps/marco.py
@@ -26,69 +26,86 @@ def app():
             fig = px.line_polar(
             pl_marco, r="Nivel", theta="Dimension",
             color="Código IE", line_close=True,
-            category_orders={'Nivel':['1A', '1B', '2A', '2B', '3', '4', '5']},
+            category_orders={'Nivel':['1A', '1B', '2A', '2B', '3', '4', '5'],
+            'Dimension':["Dimensión "+str(x) for x in range(1,9)]},
             range_r=['-1','6'], markers=True, render_mode='svg',
             color_discrete_sequence=px.colors.qualitative.Set2)
-
-            st.plotly_chart(fig, config=config)
-
-
-
-        filas = (len(IE)//2)+1
-        spfig = make_subplots(
-        cols=2,rows=filas,
-        specs=[[{"type": "polar"}]*2]*filas,
-        subplot_titles=["IE. " + str(ie) for ie in IE],
-        vertical_spacing=0.2
-    )
-    # use base go capability and copy wanted parameters from px trace
-        fila = 0
-        height = 500
-        for r, ie in enumerate(pl_marco["Código IE"].unique(),start=1):
-            pl_marcot = pl_marco.loc[pl_marco["Código IE"].eq(ie)]
-
-            if r%2 ==0:
-                col= 2
-
-            else:
-                col = 1
-                fila +=1
-                height+=500
+            if not separar:
+                st.plotly_chart(fig, config=config)
 
 
-            spfig.add_trace(
-                go.Scatterpolar(
-                    {
-                        **fig.to_dict()["data"][0],
-                        **{
-                            "r": pl_marcot["Nivel"],
-                            "theta": pl_marcot["Dimension"],
-                            "name": str(ie),
-                            "marker": {
-                                **fig.to_dict()["data"][0]["marker"],
-                                "color": pl_marcot["Código IE"]},
+        with st.container():
+            filas = (len(IE)//2)+1
+            spfig = make_subplots(
+            cols=2,rows=filas,
+            specs=[[{"type": "polar"}]*2]*filas,
+            subplot_titles=["IE. " + str(ie) for ie in IE],
+            vertical_spacing=0.2, horizontal_spacing=0.2
+        )
+        # use base go capability and copy wanted parameters from px trace
+            fila = 0
+            height = 500
+            for r, ie in enumerate(pl_marco["Código IE"].unique(),start=1):
+                pl_marcot = pl_marco.loc[pl_marco["Código IE"].eq(ie)]
+
+                if r%2 ==0:
+                    col= 2
+
+                else:
+                    col = 1
+                    fila +=1
+                    height+=500
+
+
+                spfig.add_trace(
+                    go.Scatterpolar(
+                        {
+                            **fig.to_dict()["data"][0],
+                            **{
+                                "r": pl_marcot["Nivel"],
+                                "theta": pl_marcot["Dimension"],
+                                "name": str(ie),
+                                "marker": {
+                                    **fig.to_dict()["data"][0]["marker"],
+                                    "color": pl_marcot["Código IE"]},
+                                },
                             },
-                        },
-                        fill = "toself"
-                ),
+                            fill = "toself"
+                    ),
 
-                row=fila,
-                col=col,
-            )
-            #spfig.update_layout(polar=dict(radialaxis = dict(visible = False,
-            # range = [0, 6])),
-            # showlegend = False)
-            spfig.update_polars( radialaxis=dict(categoryorder='array',
-            categoryarray= ['1A', '1B', '2A', '2B', '3', '4', '5'],
-            range=[-1,6],visible = True))
+                    row=fila,
+                    col=col,
+                )
+                #spfig.update_layout(polar=dict(radialaxis = dict(visible = False,
+                # range = [0, 6])),
+                # showlegend = False)
+                spfig.update_polars( radialaxis=dict(categoryorder='array',
+                categoryarray= ['1A', '1B', '2A', '2B', '3', '4', '5'],
+                range=[-1,6],visible = True),
+                angularaxis=dict(categoryorder='array',
+                categoryarray=["Dimensión "+str(x) for x in range(1,9)],
+                direction='clockwise'
+                ))
 
-        # finally copy across layout parameters
-        spfig = spfig.update_layout({ **fig.to_dict()["layout"], **spfig.to_dict()["layout"]})
-        spfig.layout.template = fig.layout.template
-        #for axis in ["polar","polar2","polar3"]:
-        #    spfig.layout[axis]["angularaxis"] = fig.layout.polar["angularaxis"]
+            # finally copy across layout parameters
+            spfig = spfig.update_layout({ **fig.to_dict()["layout"], **spfig.to_dict()["layout"]})
+            #spfig.layout.template = fig.layout.template
+            #for axis in ["polar","polar2","polar3"]:
+            #    spfig.layout[axis]["angularaxis"] = fig.layout.polar["angularaxis"]
 
-        # and we're done...
-        spfig.update_layout(height=height)
-
-        st.plotly_chart(spfig, use_container_width=True)
+            # and we're done...
+            spfig.update_layout(height=height, width=1000,title=dict(
+                  x=0.5,
+                  y=0.2,
+                  xanchor='center',
+                  yanchor='top',
+                  # pad = dict(
+                  #            t = 0
+                  #           ),
+                  font=dict(
+                      #family='Courier New, monospace',
+                      size=40,
+                      color='#000000'
+                  )))
+            if separar:
+                st.plotly_chart(spfig, use_container_width=False)
