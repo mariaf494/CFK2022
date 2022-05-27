84350ab344dc87445095b4b81a3f25acbff43f07
mariaf494 <mariaf494@gmail.com>
2022-05-26 22:58:42 -0500

    actualizar escala 7

--- apps/marco.py
+++ apps/marco.py
@@ -16,18 +16,36 @@ def app():
 
     IE = st.multiselect('Seleccione las instituciones que desea ver: ', list(marco['Código IE'].unique()))
 
-    marco = marco.melt(id_vars='Código IE', var_name='Dimension', value_name='Nivel')
+    marco = marco.melt(id_vars='Código IE', var_name='Dim', value_name='Nivel')
     marco['Nivel'] = pd.Categorical(marco['Nivel'], categories=['1A', '1B', '2A', '2B', '3', '4', '5'], ordered=True)
-
+    dimensiones_dict = dict(zip(["Dimensión "+str(x) for x in range(1,9)],
+    ['Liderazgo','Currículo', 'Enseñanza',
+    'Dllo profesional','EDI','Ed.Terciaria',
+    'Impacto','Género']))
+    descripciones={'Dimensión 7': "Logros<br>en cuanto a aprendizajes<br>de todos los estudiantes.",
+    'Dimensión 8':"La IE<br>ofrece igualdad<br>de oportunidades a<br>niños y niñas.",
+    'Dimensión 2':"Un plan<br>de estudios de TeI que<br>incluya pensamiento<br>computacional o ciencias<br>de la computación",
+    'Dimensión 5': "Grado en<br>que la IE es inclusiva",
+    "Dimensión 3": "Grando en<br>que estudiantes cuentan con docentes<br>que tienen el conocimiento tecnológico<br>y pedagógico del contenido necesario",
+    "Dimensión 1": "Medida en<br>que las directivas se esfuerzan por<br>articular el plan de TeI con la visión",
+    'Dimensión 4': "Medida en que<br> los y las docentes<br>tienen acceso a formación<br>profesional docente en PC",
+    "Dimensión 6": "Medida en que<br>la IE hace visibles las<br>oportunidades laborales STEM",
+    }
+    marco['Dimensión'] = marco['Dim'].replace(dimensiones_dict)
+    marco['Descripción'] = marco['Dim'].replace(descripciones)
     if len(IE)>0:
         with st.container():
             separar = st.checkbox('Ver en gráficas separadas', value=False)
             pl_marco = marco[marco['Código IE'].isin(list(IE))]
             fig = px.line_polar(
-            pl_marco, r="Nivel", theta="Dimension",
+            pl_marco, r="Nivel", theta="Dimensión",
             color="Código IE", line_close=True,
             category_orders={'Nivel':['1A', '1B', '2A', '2B', '3', '4', '5'],
-            'Dimension':["Dimensión "+str(x) for x in range(1,9)]},
+            'Dim':["Dimensión "+str(x) for x in range(1,9)],
+            'Dimensión': ['Liderazgo','Currículo','Enseñanza','Dllo profesional',
+            'EDI','Ed.Terciaria','Impacto', 'Género']},
+            hover_name='Dim',
+            hover_data=['Descripción'],
             range_r=['-1','6'], markers=True, render_mode='svg',
             color_discrete_sequence=px.colors.qualitative.Set2)
             if not separar:
@@ -63,7 +81,7 @@ def app():
                             **fig.to_dict()["data"][0],
                             **{
                                 "r": pl_marcot["Nivel"],
-                                "theta": pl_marcot["Dimension"],
+                                "theta": pl_marcot["Dimensión"],
                                 "name": str(ie),
                                 "marker": {
                                     **fig.to_dict()["data"][0]["marker"],
@@ -83,7 +101,8 @@ def app():
                 categoryarray= ['1A', '1B', '2A', '2B', '3', '4', '5'],
                 range=[-1,6],visible = True),
                 angularaxis=dict(categoryorder='array',
-                categoryarray=["Dimensión "+str(x) for x in range(1,9)],
+                categoryarray=['Liderazgo','Currículo','Enseñanza','Dllo profesional',
+                'EDI','Ed.Terciaria','Impacto', 'Género'],
                 direction='clockwise'
                 ))
 
